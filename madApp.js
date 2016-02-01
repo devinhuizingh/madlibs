6221c9c2bf7fc551d38800defb70148b4419ba3a
Devin Huizingh <devinhuizingh@gmail.com>
2016-02-01 16:17:10 -0700

    controller

---
+++ madApp.js
@@ -0,0 +1,19 @@
+angular.module('madApp', [])
+	.controller('myCtrl', function($scope) {
+		
+		$scope.female = true
+		$scope.name = 'Name'
+		$scope.jobtitle = 'Job Title'
+		$scope.tedioustask= 'Tedious Task'
+		$scope.dirtytask= 'Dirty Task'
+		$scope.celebrity='Celebrity'
+		$scope.uselessskill='Useless Skill'
+		$scope.adjective='Adjective'
+		$scope.obnoxiouscelebrity='Obnoxious Celebrity'
+		$scope.hugenumber='Huge Number'
+		$scope.formData = {};
+      
+        
+    });
+
+
